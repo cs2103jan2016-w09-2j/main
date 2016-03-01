6c1b2c691d0fae13366bb58f41b1a6a171642774
aylward <aylward_tan@hotmail.com>
2016-03-01 19:28:06 +0800

    Added main interaction

---
+++ src/tucklife/UI/interaction.java
@@ -0,0 +1,51 @@
+package tucklife.UI;
+
+import java.util.Scanner;
+import tucklife.parser.Parser;
+
+public class interaction {
+	
+	private static final String MESSAGE_COMMAND_PROMPT = "Input command";
+	private static final String MESSAGE_WELCOME = "Welcome to Tucklife!";
+	
+	private static Scanner sc = new Scanner(System.in);
+	private static Parser p = new Parser();
+	private static Storage s = new Storage();
+	private static ExternalStorage es = new ExternalStorage();
+	
+	public static void main(String[] args) {
+		
+		//initialisation
+		System.out.println(MESSAGE_WELCOME);
+		
+		//program loop
+		while (true) {
+            System.out.println(MESSAGE_COMMAND_PROMPT);
+            String command = sc.nextLine();
+            
+            ProtoTask pt = p.parse(command);
+            
+            if(pt.isError){
+            	System.out.println(pt.toString);
+            }
+            else{
+            	if(pt.getCommand().equals("save")){
+            		System.out.println(es.save(s.save()));
+            	}
+            	
+            	if(pt.getCommand().equals("load")){
+            		es.load();
+            		System.out.println(s.load(es.getLoadedLists()));
+            	}
+            	else{
+            		System.out.println(s.parseCommand(pt));
+            	}
+            }
+            
+            //String showResultToUser = getResults;
+            //System.out.println(showResultToUser);
+		}
+		
+	}
+	
+}

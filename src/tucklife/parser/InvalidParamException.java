1f2d9b23edb5f5ddc40c0442e26bedca5967c9ae
Xin Ai <lmxn0.0@gmail.com>
2016-04-10 21:15:12 +0800

    Major refactoring in Parser
    - And extra parser test cases

---
+++ src/tucklife/parser/InvalidParamException.java
@@ -0,0 +1,9 @@
+// @@author A0127835Y
+package tucklife.parser;
+
+public class InvalidParamException extends Exception {
+	
+	public InvalidParamException(String errorMsg) {
+		super(errorMsg);
+	}
+}
